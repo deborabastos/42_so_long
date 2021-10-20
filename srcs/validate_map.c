81cc4b4555432ee65ceeb358fe170c119e1e6c69
deborabastos <deborabastos14@gmail.com>
2021-10-20 14:18:47 -0300

    wall validation working. Norminette ok

--- srcs/validate_map.c
+++ srcs/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:45:38 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/10/20 11:18:26 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/10/20 11:23:21 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,7 @@ int	check_walls(t_var var)
 	while (ft_gnl(fd, &line))
 	{
 		if (row == 0)
-		{
 			check_row(line);
-		}
 		if (line[0] != '1' || line[var.map.size.x - 1] != '1')
 		{
 			perror("Lateral wall not configured");
@@ -51,9 +49,7 @@ int	check_walls(t_var var)
 		row++;
 	}
 	if (row == var.map.size.y - 1)
-	{
 		check_row(line);
-	}
 	return (0);
 }
 

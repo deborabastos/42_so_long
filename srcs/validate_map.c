42d085b43ce89023ef3fb749b4864615ebc546ed
deborabastos <deborabastos14@gmail.com>
2021-10-20 19:59:38 -0300

    validate pec

--- srcs/validate_map.c
+++ srcs/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:45:38 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/10/20 11:23:21 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/10/20 19:59:07 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,25 +50,51 @@ int	check_walls(t_var var)
 	}
 	if (row == var.map.size.y - 1)
 		check_row(line);
+	close(fd);
 	return (0);
 }
 
-// int	check_walls(t_var var)
-// {
-// 	int		fd;
-// 	char	*line;
+char	*check_pec(t_var var, char *line, char	*has)
+{
+	int		col;
 
-// 	fd = open(PATH_MAP, O_RDONLY);
-// 	while (ft_gnl(fd, &line) >= 0)
-// 	{
+	col = 0;
+	while (col <= var.map.size.x)
+	{
+		if (line[col] == 'P')
+			has[0] = 'P';
+		if (line[col] == 'E')
+			has[1] = 'E';
+		if (line[col] == 'C')
+			has[2] = 'C';
+		col++;
+	}
+	return (has);
+}
 
-// 	}
-// 	close(fd);	
-// 	return (0);
-// }
+int	check_sprites(t_var var)
+{
+	int		fd;
+	char	*has;
+	char	*line;
+
+	has = malloc(3 * sizeof(char));
+	fd = open(PATH_MAP, O_RDONLY);
+	while (ft_gnl(fd, &line) > 0)
+		check_pec(var, line, has);
+	printf("%c%c%c\n", has[0], has[1], has[2]);
+	if (has[0] == 'P' && has[1] == 'E' && has[2] == 'C')
+		printf("ok\n");
+	else
+		printf("missing\n");
+	free(has);
+	close(fd);
+	return (0);
+}
 
 int	check_map(t_var var)
 {
 	check_walls(var);
+	check_sprites(var);
 	return (0);
 }

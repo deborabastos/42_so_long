8dbf93c21ce050da8ff138f1a9eca190295b0468
deborabastos <deborabastos14@gmail.com>
2021-10-20 22:42:03 -0300

    joined check_sprites functions. Working

--- srcs/validate_map copy.c
+++ srcs/validate_map copy.c
@@ -6,7 +6,7 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:45:38 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/10/20 22:42:02 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/10/20 22:34:00 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,30 +41,40 @@ int	check_walls(t_var var)
 	return (0);
 }
 
+char	*check_pec(t_var var, char *line, char	*has)
+{
+	int		col;
+
+	col = 0;
+	while (col <= var.map.size.x)
+	{
+		if (line[col] == 'P')
+			has[0] = 'P';
+		if (line[col] == 'E')
+			has[1] = 'E';
+		if (line[col] == 'C')
+			has[2] = 'C';
+		col++;
+	}
+	return (has);
+}
+
 int	check_sprites(t_var var)
 {
 	int		fd;
-	int		has[3];
-	int		col;
+	char	*has;
 	char	*line;
 
+	has = malloc(3 * sizeof(char));
 	fd = open(PATH_MAP, O_RDONLY);
 	while (ft_gnl(fd, &line) > 0)
+		check_pec(var, line, has);
+	if (has[0] != 'P' || has[1] != 'E' || has[2] != 'C')
 	{
-		col = 0;
-		while (col <= var.map.size.x)
-		{
-			if (line[col] == 'P')
-				has[0] = 1;
-			if (line[col] == 'E')
-				has[1] = 1;
-			if (line[col] == 'C')
-				has[2] = 1;
-			col++;
-		}
-	}
-	if (has[0] != 1 || has[1] != 1 || has[2] != 1)
+		free(has);
 		error("Invalid map: misssing PEC");
+	}
+	free(has);
 	close(fd);
 	return (0);
 }

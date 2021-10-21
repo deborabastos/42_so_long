ab2799196464426b9408704b888f912c8493e6e3
deborabastos <deborabastos14@gmail.com>
2021-10-20 22:14:49 -0300

    joined check_walls functions. Working

--- srcs/validate_map.c
+++ srcs/validate_map.c
@@ -6,40 +6,37 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 22:45:38 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/10/20 21:05:23 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/10/20 22:14:24 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-int	check_row(char *line)
-{
-	while (*line)
-	{
-		if (*line != '1')
-			error("Invalid map: top or down wall not configured");
-		line++;
-	}
-	return (0);
-}
-
 int	check_walls(t_var var)
 {
 	int		fd;
 	int		row;
+	int		col;
 	char	*line;
 
 	row = 0;
 	fd = open(PATH_MAP, O_RDONLY);
-	while (ft_gnl(fd, &line))
+	while ((ft_gnl(fd, &line) >= 0) && (row < var.map.size.y))
 	{
-		if (row == 0)
-			check_row(line);
+		col = 0;
+		if (row == 0 || row == (var.map.size.y - 1))
+		{
+			while (line[col])
+			{
+				if (line[col] != '1')
+					error("Invalid map: top or down wall not configured");
+				col++;
+			}
+		}
 		if (line[0] != '1' || line[var.map.size.x - 1] != '1')
 			error("Invalid map: lateral wall not configured");
 		row++;
 	}
-	check_row(line);
 	close(fd);
 	return (0);
 }

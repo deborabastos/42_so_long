b43c28ba1a9da5e314329259a0b2fcecaf1aa36f
deborabastos <deborabastos14@gmail.com>
2021-11-04 16:59:53 -0300

    new imgs

--- srcs/free_map.c
+++ srcs/free_map.c
@@ -6,7 +6,7 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 14:59:26 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/11/04 16:26:18 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/11/04 16:59:53 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	free_map(t_var *var)
 
 	row = 0;
 	mlx_destroy_window(var->mlx, var->win);
-	// mlx_destroy_display(var->mlx);
+	mlx_destroy_display(var->mlx);
 	free(var->mlx);
 	while (row < var->map.size.y)
 	{

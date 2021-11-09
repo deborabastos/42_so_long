66b4a6aa9500aceb7897d9a106f177623fb14bc1
deborabastos <deborabastos14@gmail.com>
2021-11-09 15:03:34 -0300

    enemy animation ok

--- srcs/animation.c
+++ srcs/animation.c
@@ -6,7 +6,7 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 19:42:35 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/11/09 14:54:45 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/11/09 15:03:32 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	enemy_animation(t_var *var)
 {
 	if (var->enemy.pos.x % 2 == 0)
 		var->img.img_ptr = mlx_xpm_file_to_image(var->mlx, "./img/enemy1.xpm",
-				&var->enemy.size.x, &var->enemy.size.y);
+				&var->img.size.x, &var->img.size.y);
 	else
 		var->img.img_ptr = mlx_xpm_file_to_image(var->mlx, "./img/enemy2.xpm",
-				&var->enemy.size.x, &var->enemy.size.y);
+				&var->img.size.x, &var->img.size.y);
 	return (0);
 }
